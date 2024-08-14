db636277fd018e4d1ab177c448ec2d844af72a48
Matthew Stuewe <mstuewe@mediweb.jp>
2024-08-14 15:37:07 +0900

    Fix gtag not being loaded properly
    Unfortunately, it appears as if the tentative PR that added Google Analytics 4
    neglected to properly load gtag. This commit fixes the problem.

--- lib/event_tracker/integration/google_analytics.rb
+++ lib/event_tracker/integration/google_analytics.rb
@@ -1,19 +1,20 @@
 class EventTracker::Integration::GoogleAnalytics < EventTracker::Integration::Base
   def init
     <<-EOD
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtag/js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer', '#{@key}');
-
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
       gtag('config', '#{@key}');
+
+      (function(w,d,s,l,i){
+        w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+        var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+        j.async=true;j.src='https://www.googletagmanager.com/gtag/js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer', '#{@key}');
     EOD
   end
 
   def track(event_name, properties = {})
-    properties_js = properties.to_json
-    %Q{gtag('event', '#{event_name}', #{properties_js});}
+    %Q{gtag('event', '#{event_name}', #{properties.to_json});}
   end
 end
